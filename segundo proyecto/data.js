5d3ba61cff54cf3b5e650094a93ff77e1e341333
Ricardo Zaruma <Rjzaruma>
2022-07-10 14:13:32 -0500

    Implementacion de primera grafica, retoques de diseño

--- segundo proyecto/data.js
+++ segundo proyecto/data.js
@@ -1,5 +1,115 @@
-function cargarDatos (id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+function filtrarGraficos(){
+  region = document.getElementById("Regiones").value;
+  //console.log(region)
+  inicio=201
+  fin_starter=0
+  final=201
+  if(region == "1"){
+    inicio=1
+    final=151
+  } 
+  if(region == "2"){
+    inicio=152
+    final=251   
+  }if(region == "3"){
+    inicio=252
+    final=386
+  }if(region == "4"){
+    inicio=387
+    final=494
+  }if(region == "5"){
+    inicio=495
+    final=649
+  }if(region == "6"){
+    inicio=650
+    final=721
+  }
+  if(region == "7"){
+    inicio=722
+    final=809
+  }
+  cargarGraficos(inicio,final)
+}
+
+
+function cargarGraficos(inicio,final){  
+  water=0
+  fire=0
+  ground=0
+  electric=0
+  grass=0
+  flying=0
+  dragon=0
+  fairy=0
+  for(let i=inicio; i<=final; i++){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    .then(response => response.json())
+    .then(data => {
+      tipo=data.types[0].type.name
+      for(elem in tipo){
+        tip=tipo[elem]
+        if(tipo=="water"){
+          water++
+        }if(tipo=="fire"){
+          fire++
+        }if(tipo=="ground"){
+          ground++
+        }if(tipo=="electric"){
+          electric++
+        }if(tipo=="dragon"){
+          dragon++
+        }if(tipo=="fairy"){
+          fairy++
+        }if(tipo=="flying"){
+          flying++
+        }if(tipo=="grass"){
+          grass++
+        }
+      }
+      plantilla=`<tr>
+      <th scope="row"> Agua </th>
+      <td style="--size: ${water}/${100}; --color:aqua"> <span class="data"> ${water} </span> </td>
+      </tr>
+      <tr>
+      <th scope="row"> Fuego </th>
+      <td style="--size: ${fire}/${100}; --color:red"> <span class="data"> ${fire} </span> </td>
+      </tr>
+      <th scope="row"> Tierra </th>
+      <td style="--size: ${ground}/${100}; --color:brown"> <span class="data"> ${ground} </span> </td>
+      </tr>
+      <th scope="row"> Electrico </th>
+      <td style="--size: ${electric}/${100}; --color:yellow"> <span class="data"> ${electric} </span> </td>
+      </tr>
+      <th scope="row"> Planta </th>
+      <td style="--size: ${grass}/${100}; --color:green"> <span class="data"> ${grass} </span> </td>
+      </tr>
+      <th scope="row"> Volador </th>
+      <td style="--size: ${flying}/${100}; --color:orange"> <span class="data"> ${flying} </span> </td>
+      </tr>
+      <th scope="row"> Dragon </th>
+      <td style="--size: ${dragon}/${100}; --color:blue"> <span class="data"> ${dragon} </span> </td>
+      </tr>
+      <th scope="row"> Hada </th>
+      <td style="--size: ${fairy}/${100}; --color:pink"> <span class="data"> ${fairy} </span> </td>
+      </tr>
+        `
+      document.querySelector('.cuerpo_grafico1').innerHTML = plantilla;
+    }
+    )
+    .catch(console.error);
+  }
+}
+
+
+
+
+
+filtrarGraficos()
+//console.log(agua,fuego,planta,volador,electrico,tierra,hada,dragon)
+
+
+function cargarTabla (id) {
+     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
       .then(response => response.json())
       .then(data => {
         tipos=data.types
@@ -28,13 +138,12 @@ function cargarDatos (id) {
 
       })
       .catch(console.error);
+
 }
 
 function starters(id){
   let nom = `<option selected><p class="fs-5s">Escoga su Starter</p> </option>`;
   document.querySelector('#Starters').innerHTML = nom;
-  let selector=document.querySelector('#starter_tipos')
-  selector.textContent= `Type: `
   let selector_foto=document.getElementById('foto')
   selector_foto.src=``
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -69,11 +178,8 @@ function sprites(){
       .then(response => response.json())
       .then(data => {
         
-        tipo=data.types[0].type.name
         sprite=data.sprites.front_default
         //alert(tipo)
-        let selector=document.querySelector('#starter_tipos')
-        selector.textContent= `Type: ${tipo}`
         let selector_foto=document.getElementById('foto')
         selector_foto.src=`${sprite}`
 
@@ -90,20 +196,16 @@ function RangoPokemons(){
         </tr>`
   
   document.querySelector('.pokemons').innerHTML = datos;
-
-
-  
-
   region = document.getElementById("Regiones").value;
   //console.log(region)
-  inicio=493
+  inicio=201
   fin_starter=0
-  final=493
+  final=201
     if(region == "1"){
       id_starters=[]
       inicio=1
       fin_starter=inicio+8
-      final=151
+      final=31
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -117,7 +219,7 @@ function RangoPokemons(){
       id_starters=[]
       inicio=152
       fin_starter=inicio+8
-      final=251
+      final=182
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -130,7 +232,7 @@ function RangoPokemons(){
       id_starters=[]
       inicio=252
       fin_starter=inicio+8
-      final=386
+      final=282
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -143,7 +245,7 @@ function RangoPokemons(){
       id_starters=[]
       inicio=387
       fin_starter=inicio+8
-      final=494
+      final=417
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -156,7 +258,7 @@ function RangoPokemons(){
       id_starters=[]
       inicio=495
       fin_starter=inicio+8
-      final=649
+      final=525
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -169,7 +271,7 @@ function RangoPokemons(){
       id_starters=[]
       inicio=650
       fin_starter=inicio+8
-      final=721
+      final=680
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -183,7 +285,7 @@ function RangoPokemons(){
       id_starters=[]
       inicio=722
       fin_starter=inicio+8
-      final=809
+      final=752
       for(let i=inicio; i<=fin_starter; i++){
         id_starters.push(i)
         
@@ -193,16 +295,9 @@ function RangoPokemons(){
       starters(id_starters[3]);
       starters(id_starters[6]);
     }
-    if(region == "0"){
-      inicio=1
-      fin_starter=inicio+8
-      final=1
-      for(let i=inicio; i<=fin_starter; i++){
-        starters(i);
-      }
-    }
+    
     for(let i=inicio; i<=final; i++){
-      cargarDatos(i);
+      cargarTabla(i);
       
     }
     
